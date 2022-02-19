6a3fd678cb7d211f0c1856d4e641e3da73042e59
mkivikin <17691928+mkivikin@users.noreply.github.com>
2022-02-19 21:21:09 +0200

    added CompanyMapper tests

--- src/test/java/interview/wikicredit/mapper/CompanyMapperTest.java
+++ src/test/java/interview/wikicredit/mapper/CompanyMapperTest.java
@@ -1,11 +1,14 @@
 package interview.wikicredit.mapper;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 import interview.wikicredit.data.Company;
+import interview.wikicredit.data.WikipediaData;
 import interview.wikicredit.dto.CompanyRequest;
+import interview.wikicredit.dto.CompanyResponse;
 import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class CompanyMapperTest {
 
@@ -17,7 +20,38 @@ class CompanyMapperTest {
 
         Company company = CompanyMapper.INSTANCE.companyRequestToCompany(request);
 
-        assertNotEquals(null, company);
+        assertNotNull(company);
         assertEquals(companyName, company.getName());
+    }
+
+    @Test
+    void companyToResponse_companyWithoutWikiData_returnsCompanyResponse(){
+        String companyName = "Swedbank";
+        Company company = new Company();
+        company.setName(companyName);
+
+        CompanyResponse response = CompanyMapper.INSTANCE.companyToResponse(company);
+
+        assertNotNull(response);
+        assertEquals(companyName, response.getName());
+        assertNull(response.getSummary());
+    }
+
+    @Test
+    void companyToResponse_companyWithWikiData_returnsCompanyResponse(){
+        String companyName = "Swedbank";
+        Company company = new Company();
+        company.setId(1);
+        company.setName(companyName);
+        WikipediaData data = new WikipediaData();
+        data.setCompany(company);
+        data.setSummary("Fake summary");
+        company.setWikipediaData(data);
+
+        CompanyResponse response = CompanyMapper.INSTANCE.companyToResponse(company);
+
+        assertNotNull(response);
+        assertEquals(companyName, response.getName());
+        assertEquals(data.getSummary(), response.getSummary());
     }
 }
