a6f97fd47786218b8e8cfc52575e39fe3824cab7
mkivikin <17691928+mkivikin@users.noreply.github.com>
2022-02-20 17:22:12 +0200

    Added tests for WikipediaCompanyDataService

--- src/main/java/interview/wikicredit/data/Company.java
+++ src/main/java/interview/wikicredit/data/Company.java
@@ -1,7 +1,6 @@
 package interview.wikicredit.data;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
--- src/test/java/interview/wikicredit/service/WikipediaCompanyDataServiceTest.java
+++ src/test/java/interview/wikicredit/service/WikipediaCompanyDataServiceTest.java
@@ -1,2 +1,67 @@
-package interview.wikicredit.service;public class WikipediaCompanyDataServiceTest {
+package interview.wikicredit.service;
+
+import interview.wikicredit.data.Company;
+import interview.wikicredit.data.WikipediaData;
+import interview.wikicredit.repository.WikipediaDataRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.persistence.EntityNotFoundException;
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class WikipediaCompanyDataServiceTest {
+
+    private WikipediaCompanyDataService service;
+
+    @Mock
+    private DefaultCompanyService companyService;
+
+    @Mock
+    WikipediaRequestService requestService;
+
+    @Mock
+    WikipediaDataRepository repository;
+
+    @BeforeEach
+    private void setUp(){
+        service = new WikipediaCompanyDataService(requestService, companyService, repository);
+    }
+
+    @Test
+    void getCompanyData_notFound_throwsEntityNotFoundException(){
+        Integer companyId = 1;
+
+        Mockito.when(repository.findById(companyId)).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () -> service.getCompanyData(companyId));
+    }
+
+    @Test
+    void getCompanyData_found_ReturnsWikipediaData(){
+        Integer companyId = 1;
+        Company company = new Company();
+        company.setId(companyId);
+        company.setName("Zoolander Enterprises");
+        WikipediaData data = new WikipediaData();
+        data.setCompany(company);
+        data.setSummary("But why male models?");
+        data.setPageId(213);
+        data.setUpdatedAt(Instant.now());
+        data.setArticleExists(true);
+
+        Mockito.when(repository.findById(companyId)).thenReturn(Optional.of(data));
+
+        WikipediaData actualData = service.getCompanyData(companyId);
+
+        assertEquals(data, actualData);
+    }
 }
